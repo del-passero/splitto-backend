695e7ff7f560e6df21beaf329b3937534e12b725
del-passero <ap.vorobyev@gmail.com>
2025-06-26 20:05:10 +0300

    еще немного допилил backend под авторизацию telegram

--- src/main.py
+++ src/main.py
@@ -25,7 +25,10 @@ app = FastAPI(
 # --- Настройка CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://splitto.app"]
+    allow_origins=[
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://splitto.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
