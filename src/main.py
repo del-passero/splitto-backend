ceba414b30d115fa23dbfe3cfb8fec9cfadfbb3b
del-passero <ap.vorobyev@gmail.com>
2025-07-07 23:51:28 +0300

    модель юзера изменена

--- src/main.py
+++ src/main.py
@@ -32,10 +32,11 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "http://localhost:5173",
         "http://127.0.0.1:3000",
+        "http://127.0.0.1:5173",
         "https://splitto.app",
-        "https://www.splitto.app",
-        "https://splitto-backend-prod-ugraf.amvera.io"
+        "https://www.splitto.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
